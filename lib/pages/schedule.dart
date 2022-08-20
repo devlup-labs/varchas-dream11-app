a9f89eafb572dc79534eaaade203b56c114423e4
Anadi Sharma <sharma.130@iitj.ac.in>
2022-08-21 00:47:07 +0530

    Schedule Added

--- lib/pages/schedule.dart
+++ lib/pages/schedule.dart
@@ -1,10 +1,200 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
+import 'package:varchas_dream_11/utilities/widgets.dart';
+import 'package:varchas_dream_11/utilities/constants.dart';
+import 'package:http/http.dart' as http;
 
-class Schedule extends StatelessWidget {
+class Schedule extends StatefulWidget {
   const Schedule({Key? key}) : super(key: key);
+  @override
+  State<Schedule> createState() => _MyAppState();
+}
 
+class _MyAppState extends State<Schedule> {
+  final sportsChoices = {
+    '1': 'Athletics',
+    '2': 'Badminton',
+    '3': 'Basketball',
+    '4': 'Chess',
+    '5': 'Cricket',
+    '6': 'Football',
+    '7': 'Table Tennis',
+    '8': 'Tennis',
+    '9': 'Volleyball',
+    '10': 'Badminton-Mixed doubles',
+  };
+  final dates = ["2022-04-07", "2022-04-08", "2022-04-09"];
+  Future getMatchData(int a) async {
+    var response1 = await http.get(Uri.parse("https://varchas22.in/events/MatchApi/"));
+    var response2 = await http.get(Uri.parse("https://varchas22.in/registration/teamsApi/"));
+    if (response1.statusCode != 200){
+      throw Exception('Failed to load data');
+    }
+    if (response2.statusCode != 200){
+      throw Exception('Failed to load data');
+    }
+    Map jsonData1 = jsonDecode(response1.body) as Map<String,dynamic>;
+    Map jsonData2 = jsonDecode(response2.body) as Map<String,dynamic>;
+    List<Teams> teamNames = [];
+    List<SchMatches> schMatchList1 = [];
+    List<SchMatches> schMatchList2 = [];
+    List<SchMatches> schMatchList3 = [];
+    for (var v in jsonData2["results"]){
+      Teams teamDetails = Teams(
+          v["url"],v["teamId"],v["college"],v["sport"],v["score"]
+      );
+      teamNames.add(teamDetails);
+    }
+    if (jsonData2["next"]!=null){
+      while(jsonData2["next"]!=null){
+        var url = Uri.parse(jsonData2["next"]);
+        var response = await http.get(url);
+        jsonData2 = jsonDecode(response.body);
+        for (var v in jsonData2["results"]){
+          Teams teamDetails = Teams(
+              v["url"],v["teamId"],v["college"],v["sport"],v["score"]
+          );
+          teamNames.add(teamDetails);
+        }
+      }
+    }
+    var teamDict = {};
+    for (var v in teamNames){
+      teamDict[v.teamId]=v.college;
+    }
+    for (var u in jsonData1["results"]) {
+      SchMatches schMatch = SchMatches(
+          sportsChoices[u["event"]], teamDict[u["team1"]],teamDict[u["team2"]], u["date"], u["time"],u["team1Logo"]="",u["team2Logo"]="",u["matchWinner"]="",u["pointsEarned"]="",u["scoreDifference"]=""
+      );
+      if (schMatch.date==dates[0]){
+        schMatchList1.add(schMatch);
+      }
+      else if (schMatch.date==dates[1]){
+        schMatchList2.add(schMatch);
+      }
+      else {
+        schMatchList3.add(schMatch);
+      }
+    }
+    for (var i=0 ; i<schMatchList1.length;i++){
+      schMatchList1[i].date = schMatchList1[i].date?.substring(2);
+      schMatchList1[i].time = schMatchList1[i].time?.substring(0,5);
+    }
+    for (var i=0 ; i<schMatchList2.length;i++){
+      schMatchList2[i].date = schMatchList2[i].date?.substring(2);
+      schMatchList2[i].time = schMatchList2[i].time?.substring(0,5);
+    }
+    for (var i=0 ; i<schMatchList3.length;i++){
+      schMatchList3[i].date = schMatchList3[i].date?.substring(2);
+      schMatchList3[i].time = schMatchList3[i].time?.substring(0,5);
+    }
+    if (a==1) {
+      return schMatchList1;
+    } else if (a==2) {
+      return schMatchList2;
+    } else {
+      return schMatchList3;
+    }
+  }
   @override
   Widget build(BuildContext context) {
-    return const Center(child: Text("Schedule"));
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          body: Column(
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(28, 0, 28, 0),
+                decoration: const BoxDecoration(
+                    color: secondaryColor,
+                    borderRadius: BorderRadius.only(
+                        bottomRight: Radius.circular(10),
+                        bottomLeft: Radius.circular(10))
+                ),
+                child: const TabBar(
+                  tabs: [
+                    Tab(child: Text("Day-1")),
+                    Tab(child: Text("Day-2")),
+                    Tab(child: Text("Day-3")),
+                  ],
+                ),
+                height: 53,
+              ),
+              Container(
+                margin: const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                width: double.maxFinite,
+                height: MediaQuery.of(context).size.height - 193,
+                child: TabBarView(
+                    children: [
+                      FutureBuilder<dynamic>(
+                          future: getMatchData(1),
+                          builder: (context, snapshot){
+                            if (snapshot.data == null){
+                              return const Center(
+                                child : CircularProgressIndicator(),
+                              );
+                            } else {
+                              return matchesOfDay(snapshot.data,context);
+                            }
+                          }),
+                      FutureBuilder<dynamic>(
+                          future: getMatchData(2),
+                          builder: (context, snapshot){
+                            if (snapshot.data == null){
+                              return const Center(
+                                child : CircularProgressIndicator(),
+                              );
+                            } else {
+                              return matchesOfDay(snapshot.data,context);
+                            }
+                          }),
+                      FutureBuilder<dynamic>(
+                      future: getMatchData(3),
+                      builder: (context, snapshot){
+                        if (snapshot.data == null){
+                          return const Center(
+                            child : CircularProgressIndicator(),
+                          );
+                        } else {
+                          return matchesOfDay(snapshot.data,context);
+                        }
+                    }),
+                    ]
+                ),
+              )
+            ],
+          ),
+          backgroundColor: primaryColor,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
+    );
   }
+}
+
+Widget matchesOfDay(List matchList,BuildContext context){
+  return SingleChildScrollView(child: Column(
+    children: [
+      Container(
+        height: 10,
+      ),
+      for (var i=0; i<matchList.length;i++)
+        matchDisplay(i, matchList, context),
+      Container(
+        height: 10,
+      )
+    ],
+  ),
+    );
+}
+
+class SchMatches {
+  String? event,team1,team2,date,time,team1Logo,team2Logo,matchWinner,pointsEarned,scoreDifference;
+  SchMatches(this.event, this.team1, this.team2, this.date, this.time,this.team1Logo,this.team2Logo,this.matchWinner,this.pointsEarned,this.scoreDifference);
+}
+class Teams{
+  String? teamId,college,url,sport;
+  int score;
+  Teams(this.url, this.teamId, this.college, this.sport, this.score);
 }
--- lib/utilities/widgets.dart
+++ lib/utilities/widgets.dart
@@ -3,105 +3,117 @@ import 'package:flutter_profile_picture/flutter_profile_picture.dart';
 import 'package:flutter/material.dart';
 
 Widget matchDisplay(int a,dynamic matches,BuildContext context) => Container (
-  margin:const EdgeInsets.fromLTRB(0, 5, 0, 5),
-  padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
-  width: 380,
-  decoration: const BoxDecoration(
+    margin:const EdgeInsets.fromLTRB(0, 6, 0, 5),
+    padding: const EdgeInsets.fromLTRB(0, 5, 0, 10),
+    width: 380,
+    decoration: const BoxDecoration(
     borderRadius: BorderRadius.all(Radius.circular(12)),
-    color: primaryColorLight,
-  ),
-  child: Wrap(
-    children: [
-      Container(
-        padding: const EdgeInsets.fromLTRB(25, 0, 25, 3),
-        width: 380,
-        child: Center(child: Wrap(
-          children: [
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
-              child:Text(
-                matches[a].date,
-                style: const TextStyle(
-                    color: Colors.white
-                ),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 3, 0, 0),
-              child:SizedBox(
-                child: Center(
-                  child: sportsname(matches[a].event),
-                ),
-                width: MediaQuery.of(context).size.width*0.55,
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 8, 0, 0),
-              child:Text(
-                matches[a].time,
-                style: const TextStyle(
-                    color: Colors.white
-                ),
-              ),
-            ),
-          ],
-        ),),
-      ),
-      const Divider(
-        indent: 10,
-        endIndent: 10,
-        color: Colors.white,
-        thickness: 1,
-      ),
-      Center(
-        child: Wrap(
-          children: [
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-              child: Column(
-                children: [
-                  teamlogo(matches[a].team1Logo,matches[a].team1),
-                  Text(
-                    matches[a].team1,
-                    style: kTeamNameTextStyleWhite,
+      color: primaryColorLight,
+    ),
+    child: Wrap(
+      children: [
+        Container(
+          padding: const EdgeInsets.fromLTRB(25, 0, 25, 3),
+          width: 380,
+          child: Center(child: Wrap(
+            children: [
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child:Text(
+                  matches[a].date,
+                  style: const TextStyle(
+                      color: Colors.white
                   ),
-                ],
+                ),
               ),
-            ),
-            Container(
-              width: MediaQuery.of(context).size.width*0.38,
-              height: 100,
-              child: Center(
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 3, 0, 0),
+                child:SizedBox(
+                  child: Center(
+                    child: sportsname(matches[a].event),
+                  ),
+                  width: MediaQuery.of(context).size.width*0.55,
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 8, 0, 0),
+                child:Text(
+                  matches[a].time,
+                  style: const TextStyle(
+                      color: Colors.white
+                  ),
+                ),
+              ),
+            ],
+          ),),
+        ),
+        const Divider(
+          indent: 10,
+          endIndent: 10,
+          color: Colors.white,
+          thickness: 1,
+        ),
+        Center(
+          child: Wrap(
+            children: [
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
                 child: Column(
-                  children: const [
-                    Text(
-                      "    V/S",
+                  children: [
+                    teamlogo(matches[a].team1Logo,matches[a].team1),
+                    Container(
+                      width: 90,
+                      child: Text(
+                      matches[a].team1,
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
                       style: kTeamNameTextStyleWhite,
+                        textAlign: TextAlign.center,
+                    ),
                     )
                   ],
                 ),
               ),
-              padding: const EdgeInsets.fromLTRB(0, 40, 0, 25),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-              child: Column(
-                children: [
-                  teamlogo(matches[a].team2Logo,matches[a].team2),
-                  Text(
-                    matches[a].team2,
-                    style: kTeamNameTextStyleWhite,
+              Container(
+                width: MediaQuery.of(context).size.width*0.38,
+                height: 100,
+                child: Center(
+                  child: Column(
+                    children: const [
+                      Text(
+                        "V/S",
+                        style: kTeamNameTextStyleWhite,
+                        textAlign: TextAlign.center,
+                      )
+                    ],
                   ),
-                ],
+                ),
+                padding: const EdgeInsets.fromLTRB(0, 40, 0, 25),
               ),
-            ),
-          ],
+              Container(
+                padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                child: Column(
+                  children: [
+                    teamlogo(matches[a].team2Logo,matches[a].team2),
+                    Container(
+                      width: 90,
+                      child: Text(
+                      matches[a].team2,
+                      maxLines: 2,
+                      overflow: TextOverflow.ellipsis,
+                      style: kTeamNameTextStyleWhite,
+                        textAlign: TextAlign.center,
+                    ),)
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
-      ),
-      resultDisplay(matches[a].matchWinner,matches[a].pointsEarned,matches[a].scoreDifference,matches[a].event,matches[a].team1,matches[a].team2),
-    ],
-  ),
-);
+        resultDisplay(matches[a].matchWinner,matches[a].pointsEarned,matches[a].scoreDifference,matches[a].event,matches[a].team1,matches[a].team2),
+      ],
+    ),
+  );
 Widget resultDisplay(String winner,String points,String scoreDifference,String sport,String team1, String team2) {
   if (winner!=""){
     return Column(
